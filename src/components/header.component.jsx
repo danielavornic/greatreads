2edd117f32d477a5f9177d3cbcbf53a3cd96d294
Daniela Vornic <daniela.vornic@gmail.com>
2021-08-05 19:02:02 +0300

    add Chakra UI library and create header component

---
+++ src/components/header.component.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import {
+  chakra,
+  Flex,
+  useColorModeValue,
+  VisuallyHidden,
+  HStack,
+  Button
+} from '@chakra-ui/react';
+
+import { ReactComponent as Logo } from '../assets/greatreads-logo.svg';
+
+const Header = () => {
+  const bg = useColorModeValue('white', 'gray.800');
+
+  return (
+    <React.Fragment>
+      <chakra.header
+        bg={bg}
+        w='full'
+        px={{ base: 4, md: 8, lg: 10 }}
+        py={4}
+      >
+        <Flex alignItems='center' justifyContent='space-between' mx='auto'>
+          <Flex>
+            <chakra.a
+              href='/'
+              title='greatreads homepage'
+              display='flex'
+              alignItems='center'
+            >
+                <Logo />
+              <VisuallyHidden>greatreads</VisuallyHidden>
+            </chakra.a>
+          </Flex>
+          <HStack display='flex' alignItems='center' spacing={1}>
+            <HStack
+              spacing={1}
+              mr={1}
+              color='brand.500'
+              display={{ base: 'inline-flex' }}
+            >
+              <Button variant='ghost' size='sm'>Sign in</Button>
+            </HStack>
+            <Button colorScheme='brand' size='sm'>
+              Sign up
+            </Button>
+          </HStack>
+        </Flex>
+      </chakra.header>
+    </React.Fragment>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  brand: {
+    50: '#ecefff',
+    100: '#cbceeb',
+    200: '#a9aed6',
+    300: '#888ec5',
+    400: '#666db3',
+    500: '#4d5499',
+    600: '#3c4178',
+    700: '#2a2f57',
+    800: '#181c37',
+    900: '#080819'
+  }
+};
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false
+};
+
+const theme = extendTheme({ colors, config });
+
+const rootElement = document.getElementById('root');
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ChakraProvider>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
