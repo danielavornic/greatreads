610721c7281f10d81194bd36ac76fee6f08d6d1a
Daniela Vornic <daniela.vornic@gmail.com>
2021-08-20 12:52:27 +0300

    delete console.log(searchResults)

--- src/redux/library/library.sagas.js
+++ src/redux/library/library.sagas.js
@@ -10,7 +10,6 @@ export function* searchAsync({ payload: { category, query } }) {
     const urlCategory = (category === 'all') ? 'q' : category;
     const response = yield fetch(`https://openlibrary.org/search.json?${urlCategory}=${formattedQuery}`);
     const searchResults = yield response.json();
-    console.log(searchResults);
     yield put(searchSuccess(searchResults));
   } catch(error) {
     yield put(searchFailure(error.message))
