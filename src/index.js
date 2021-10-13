65a936cb9f82aa67aee32d2f2b64a6935cffd7ed
Daniela Vornic <daniela.vornic@gmail.com>
2021-10-13 23:05:34 +0300

    move theme properties in a separate file

--- src/index.js
+++ src/index.js
@@ -6,39 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
 import { store, persistor } from './redux/store';
 
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-
-import '@fontsource/lora';
-
-const colors = {
-  brand: {
-    25: '#f5f6ff',
-    50: '#ecefff',
-    100: '#cbceeb',
-    200: '#a9aed6',
-    300: '#888ec5',
-    400: '#666db3',
-    500: '#4d5499',
-    600: '#3c4178',
-    700: '#2a2f57',
-    800: '#181c37',
-    900: '#080819'
-  }
-};
-
-const fonts = {
-  heading: 'Lora'
-};
-
-const config = {
-  initialColorMode: 'light',
-  useSystemColorMode: false
-};
-
-const theme = extendTheme({ colors, fonts, config });
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
 
 const rootElement = document.getElementById('root');
 
---
+++ src/theme.js
@@ -0,0 +1,32 @@
+import { extendTheme } from '@chakra-ui/react';
+
+import '@fontsource/lora';
+
+const colors = {
+  brand: {
+    25: '#f5f6ff',
+    50: '#ecefff',
+    100: '#cbceeb',
+    200: '#a9aed6',
+    300: '#888ec5',
+    400: '#666db3',
+    500: '#4d5499',
+    600: '#3c4178',
+    700: '#2a2f57',
+    800: '#181c37',
+    900: '#080819'
+  }
+};
+
+const fonts = {
+  heading: 'Lora'
+};
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false
+};
+
+const theme = extendTheme({ colors, fonts, config });
+
+export default theme;
