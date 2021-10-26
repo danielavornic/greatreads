55a474080bcd5e48dcf96a5ae159ded52100fbba
Daniela Vornic <daniela.vornic@gmail.com>
2021-10-26 14:22:03 +0300

    underline footer link on hover

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -6,7 +6,6 @@ import {
   Container,
   Stack,
   Text,
-  useColorModeValue,
   VisuallyHidden,
 } from '@chakra-ui/react';
 
@@ -16,7 +15,7 @@ import { ReactComponent as Logo } from '../../assets/greatreads-logo.svg';
 
 const SocialButton = ({ children, label, href }) => (
   <chakra.button
-    bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+    bg={'blackAlpha.100'}
     rounded={'full'}
     w={8}
     h={8}
@@ -29,7 +28,7 @@ const SocialButton = ({ children, label, href }) => (
     justifyContent={'center'}
     transition={'background 0.3s ease'}
     _hover={{
-      bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      bg: 'blackAlpha.200',
     }}
   >
     <VisuallyHidden>{label}</VisuallyHidden>
@@ -38,10 +37,7 @@ const SocialButton = ({ children, label, href }) => (
 );
 
 const Footer = () => (
-  <Box
-    bg={useColorModeValue('gray.50', 'gray.900')}
-    color={useColorModeValue('gray.700', 'gray.200')}
-  >
+  <Box bg={'gray.50'} color={'gray.700'}>
     <Container
       as={Stack}
       maxW={'6xl'}
@@ -53,17 +49,25 @@ const Footer = () => (
     >
       <Logo />
       <Stack direction={'row'} spacing={6}>
-        <Link to='/'>Home</Link>
-        <Link to='/books'>Books</Link>
-        <Link to='/genres'>Genres</Link>
-        <Link to='/authors'>Authors</Link>
+        <Link to='/'>
+          <Text _hover={{ textDecoration: 'underline' }}>Home</Text>
+        </Link>
+        <Link to='/books'>
+          <Text _hover={{ textDecoration: 'underline' }}>Books</Text>
+        </Link>
+        <Link to='/genres'>
+          <Text _hover={{ textDecoration: 'underline' }}>Genres</Text>
+        </Link>
+        <Link to='/authors'>
+          <Text _hover={{ textDecoration: 'underline' }}>Authors</Text>
+        </Link>
       </Stack>
     </Container>
 
     <Box
       borderTopWidth={1}
       borderStyle={'solid'}
-      borderColor={useColorModeValue('gray.200', 'gray.700')}
+      borderColor={'gray.200'}
     >
       <Container
         as={Stack}
