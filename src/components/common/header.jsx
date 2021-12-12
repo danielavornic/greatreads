de7530d091cfe4c2dc51d11167d327e6be8d492a
Daniela Vornic <daniela.vornic@gmail.com>
2021-12-12 19:34:07 +0200

    add sign out link and avatar in header when user is logged in

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,55 +1,75 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import { Link } from 'react-router-dom';
-
 import {
-  chakra,
-  Flex,
-  useColorModeValue,
-  VisuallyHidden,
-  HStack,
-  Button
+	chakra,
+	Flex,
+	VisuallyHidden,
+	HStack,
+	Button,
+	Avatar,
 } from '@chakra-ui/react';
 
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
 import { ReactComponent as Logo } from '../../assets/greatreads-logo.svg';
 
-const Header = () => {
-  const bg = useColorModeValue('white', 'gray.800');
+const Header = ({ user, signOutStart }) => (
+	<chakra.header
+		bg={'white'}
+		w='full'
+		px={{ base: 4, md: 8, lg: 10 }}
+		py={4}
+		borderBottom='1px'
+		borderColor='gray.200'
+	>
+		<Flex alignItems='center' justifyContent='space-between' mx='auto'>
+			<Flex>
+				<Link to='/'>
+					<Logo />
+					<VisuallyHidden>greatreads</VisuallyHidden>
+				</Link>
+			</Flex>
+			<HStack display='flex' alignItems='center' spacing={1}>
+				{user ? (
+					<HStack>
+						<Link to='/'>
+							<Button variant='ghost' size='sm' onClick={signOutStart}>
+								Sign out
+							</Button>
+						</Link>
+						<Avatar
+							size='sm'
+              bg={'brand.500'}
+							name={user.displayName}
+							src={user.photoURL ? user.photoURL : ''}
+						/>
+					</HStack>
+				) : (
+					<HStack>
+						<Link to='/signin'>
+							<Button variant='ghost' size='sm'>
+								Sign in
+							</Button>
+						</Link>
+						<Link to='/signup'>
+							<Button colorScheme='brand' size='sm'>
+								Sign up
+							</Button>
+						</Link>
+					</HStack>
+				)}
+			</HStack>
+		</Flex>
+	</chakra.header>
+);
 
-  return (
-    <chakra.header
-      bg={bg}
-      w='full'
-      px={{ base: 4, md: 8, lg: 10 }}
-      py={4}
-      borderBottom="1px"
-      borderColor="gray.200"
-    >
-      <Flex alignItems='center' justifyContent='space-between' mx='auto'>
-        <Flex>
-          <Link to='/'>
-            <Logo />
-            <VisuallyHidden>greatreads</VisuallyHidden>
-          </Link>
-        </Flex>
-        <HStack display='flex' alignItems='center' spacing={1}>
-          <HStack
-            spacing={1}
-            mr={1}
-            color='brand.500'
-            display={{ base: 'inline-flex' }}
-          >
-            <Link to='/signin'>
-              <Button variant='ghost' size='sm'>Sign in</Button>
-            </Link>
-          </HStack>
-          <Link to='/signup'>
-            <Button colorScheme='brand' size='sm'>
-              Sign up
-            </Button>
-          </Link>
-        </HStack>
-      </Flex>
-    </chakra.header>
-  );
-};
+const mapStateToProps = createStructuredSelector({
+	user: selectCurrentUser,
+});
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+	signOutStart: () => dispatch(signOutStart()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
