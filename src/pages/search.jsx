0a3838a9c7999aaf661bbf5b4adbcd564cdfbd20
Daniela Vornic <daniela.vornic@gmail.com>
2021-12-20 19:46:05 +0200

    display searchResultsContainer only when searching has already started

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,31 +1,27 @@
 import { withRouter } from 'react-router';
 
-import {
-  Container,
-  Heading,
-  Box
-} from '@chakra-ui/layout';
+import { Container, Heading, Box } from '@chakra-ui/layout';
 
 import SearchInput from '../components/search/search-input';
 import SearchResults from '../components/search/search-results';
 
 const SearchPage = ({ match }) => (
-  <Container maxW={'5xl'} my='48px'>
-    <Heading
-      as='h1'
-      textAlign='center'
-      fontWeight={700}
-      fontSize={{ base: '3xl', md: '4xl' }}
-      textTransform='capitalize'
-      pb={['28px', '36px']}
-    >
-      Search {match.params.category}
-    </Heading>
-    <Box align='center'>
-      <SearchInput inputCategory={match.params.category} />
-    </Box>
-    <SearchResults />
-  </Container>
+	<Container maxW={'5xl'} my='48px'>
+		<Heading
+			as='h1'
+			textAlign='center'
+			fontWeight={700}
+			fontSize={{ base: '3xl', md: '4xl' }}
+			textTransform='capitalize'
+			pb={['28px', '36px']}
+		>
+			Search {match.params.category}
+		</Heading>
+		<Box align='center'>
+			<SearchInput inputCategory={match.params.category} />
+		</Box>
+		{match.params.facet || match.params.term ? <SearchResults /> : null}
+	</Container>
 );
 
 export default withRouter(SearchPage);
