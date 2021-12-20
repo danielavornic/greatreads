716213bc4d208b21a3b26a170f1605f74a287ec2
Daniela Vornic <daniela.vornic@gmail.com>
2021-12-20 20:47:00 +0200

    add fallback author image on loading

--- src/pages/author.jsx
+++ src/pages/author.jsx
@@ -1,112 +1,111 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { fetchAuthorStart } from '../redux/authors/authors.actions';
-import { selectAuthor, selectIsAuthorFetching } from '../redux/authors/authors.selectors';
-
 import {
-  Container,
-  Box,
-  Heading,
-  Text
-} from '@chakra-ui/layout';
-import { 
-  Flex,
-  Image 
-} from '@chakra-ui/react';
+	selectAuthor,
+	selectIsAuthorFetching,
+} from '../redux/authors/authors.selectors';
+
+import { Container, Box, Heading, Text } from '@chakra-ui/layout';
+import { Flex, Image } from '@chakra-ui/react';
 import CustomSpinner from '../components/common/custom-spinner';
 import ReadMore from '../components/common/read-more';
 import AuthorBooksContainer from '../components/author/author-books-container';
 
 const AuthorPage = ({ match, fetchAuthorStart, author, isAuthorLoading }) => {
-  const authorKey = match.params.authorKey;
-  useEffect(() => {
-    fetchAuthorStart(authorKey);
-  }, [authorKey, fetchAuthorStart]);
+	const authorKey = match.params.authorKey;
+	useEffect(() => {
+		fetchAuthorStart(authorKey);
+	}, [authorKey, fetchAuthorStart]);
 
-  return (
-    <Container maxW={'5xl'} my='48px'>
-      {
-        author
-        ? author.error === 'notfound'
-          ? <Text align='center'>Author not found</Text>
-          : <Flex flexDirection={['column', 'column', 'row']}>
-              <Box 
-                w={['full', 'full', '210px']} 
-                mr={[4, 6, 8]}
-                mb={8}
-              >
-                <Image 
-                  src={
-                    author.photos && author.photos[0] !== -1 
-                    ? `https://covers.openlibrary.org/a/id/${author.photos[0]}-L.jpg`
-                    : 'https://openlibrary.org/images/icons/avatar_author-lg.png'
-                  } 
-                  width='200px'
-                  height='auto'
-                  boxShadow='lg'
-                />
-              </Box>
+	const [ isLoadingImg, setIsLoadingImg ] = useState(true);
 
-              <Box w={'full'}>
-                  <Heading
-                    as='h1'
-                    fontWeight={700}
-                    fontSize={{ base: '3xl', md: '4xl' }}
-                    pb='12px'
-                  >
-                    {author.name}
-                  </Heading>
+	return (
+		<Container maxW={'5xl'} my='48px'>
+			{author ? (
+				author.error === 'notfound' ? (
+					<Text align='center'>Author not found</Text>
+				) : (
+					<Flex flexDirection={['column', 'column', 'row']}>
+						<Box w={['full', 'full', '210px']} mr={[4, 6, 8]} mb={8}>
+							<Image
+								src='https://openlibrary.org/images/icons/avatar_author-lg.png'
+								width='200px'
+								height='auto'
+								boxShadow='lg'
+								alt='fallback-author-image'
+								display={isLoadingImg ? 'block' : 'none'}
+							/>
+							<Image
+								src={
+									author.photos && author.photos[0] !== -1
+										? `https://covers.openlibrary.org/a/id/${author.photos[0]}-L.jpg`
+										: 'https://openlibrary.org/images/icons/avatar_author-lg.png'
+								}
+								width='200px'
+								height='auto'
+								boxShadow='lg'
+								alt='author-image'
+								onLoad={() => setIsLoadingImg(false)}
+								display={isLoadingImg ? 'none' : 'block'}
+							/>
+						</Box>
 
-                  {
-                    author.birth_date || author.death_date
-                    ? <Text fontSize={{ base: 'lg', md: 'xl' }} pb='20px'>
-                        { author.birth_date ? `${author.birth_date} - ` : '? - ' }
-                        { author.death_date ? `${author.death_date}` : '' }
-                      </Text>
-                    : null
-                  }
+						<Box w={'full'}>
+							<Heading
+								as='h1'
+								fontWeight={700}
+								fontSize={{ base: '3xl', md: '4xl' }}
+								pb='12px'
+							>
+								{author.name}
+							</Heading>
 
-                  {
-                    author.bio 
-                    ? author.bio.value 
-                      ? <ReadMore text={author.bio.value} />
-                      : <ReadMore text={author.bio} />
-                    : null
-                  }
-                
-                <Box mt={[8, 10]}>
-                  <Heading
-                    as='h2'
-                    fontSize={{ base: 'xl', md: '2xl' }}
-                    pb='20px'
-                  >
-                    Books
-                  </Heading>
-                  <AuthorBooksContainer/>
-                </Box>
-              </Box>
-            </Flex>
-        : isAuthorLoading
-          ? <CustomSpinner/>
-        : <Text align='center'>Author not found</Text>
-      }
-    </Container>
-  )
+							{author.birth_date || author.death_date ? (
+								<Text fontSize={{ base: 'lg', md: 'xl' }} pb='20px'>
+									{author.birth_date ? `${author.birth_date} - ` : '? - '}
+									{author.death_date ? `${author.death_date}` : ''}
+								</Text>
+							) : null}
+
+							{author.bio ? (
+								author.bio.value ? (
+									<ReadMore text={author.bio.value} />
+								) : (
+									<ReadMore text={author.bio} />
+								)
+							) : null}
+
+							<Box mt={[8, 10]}>
+								<Heading as='h2' fontSize={{ base: 'xl', md: '2xl' }} pb='20px'>
+									Books
+								</Heading>
+								<AuthorBooksContainer />
+							</Box>
+						</Box>
+					</Flex>
+				)
+			) : isAuthorLoading ? (
+				<CustomSpinner />
+			) : (
+				<Text align='center'>Author not found</Text>
+			)}
+		</Container>
+	);
 };
 
 const mapStateToProps = createStructuredSelector({
-  author: selectAuthor,
-  isAuthorLoading: selectIsAuthorFetching
+	author: selectAuthor,
+	isAuthorLoading: selectIsAuthorFetching,
 });
 
-const mapDispatchToProps = dispatch => ({
-  fetchAuthorStart: (authorKey) => dispatch(fetchAuthorStart(authorKey))
+const mapDispatchToProps = (dispatch) => ({
+	fetchAuthorStart: (authorKey) => dispatch(fetchAuthorStart(authorKey)),
 });
 
 export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)
-  (AuthorPage)
+	connect(mapStateToProps, mapDispatchToProps)(AuthorPage)
 );
