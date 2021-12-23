9c06891d127b49c3d59fbd8bf3e93e1ff0160035
Daniela Vornic <daniela.vornic@gmail.com>
2021-12-23 11:08:11 +0200

    format books sagas with prettier

--- src/redux/books/books.sagas.js
+++ src/redux/books/books.sagas.js
@@ -1,36 +1,34 @@
 import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import BooksActionTypes from './books.types';
-
 import { fetchBookSuccess, fetchBookFailure } from './books.actions';
 
 export function* fetchBookAsync({ payload }) {
   try {
     const response = yield fetch(`https://openlibrary.org/api/books?bibkeys=OLID:${payload}&jscmd=details&format=json`);
     const json = yield response.json();
-    const book = yield json[Object.keys(json)[0]].details;
+    const book = yield json[ Object.keys(json)[ 0 ] ].details;
 
     if (book.works) {
-      const worksResponse = yield fetch(`https://openlibrary.org${book.works[0].key}.json`);
+      const worksResponse = yield fetch(`https://openlibrary.org${book.works[ 0 ].key}.json`);
       const works = yield worksResponse.json();
-      const description = yield works.description 
-                              ? works.description.value 
-                                ? works.description.value 
-                                : works.description
-                              : '';
+      const description = yield works.description
+        ? works.description.value
+          ? works.description.value
+          : works.description
+        : '';
       book.description = description;
-      
+
       if (!book.authors && !book.by_statement && works.authors) {
-        const authorResponse = yield fetch(`https://openlibrary.org${works.authors[0].author.key}.json`);
+        const authorResponse = yield fetch(`https://openlibrary.org${works.authors[ 0 ].author.key}.json`);
         const author = yield authorResponse.json();
-        book.authors = [{name: author.name}];
+        book.authors = [ { name: author.name } ];
       }
     } else {
       book.description = '';
     }
-    
     yield put(fetchBookSuccess(book));
-  } catch(error) {
+  } catch (error) {
     yield put(fetchBookFailure(error.message));
   }
 }
