cad12640d642219d5787e5dff2a200abc0cb69f0
Daniela Vornic <daniela.vornic@gmail.com>
2021-12-24 23:28:52 +0200

    separate books sagas into multiple functions and format code

--- src/redux/books/books.sagas.js
+++ src/redux/books/books.sagas.js
@@ -1,120 +1,150 @@
 import { takeLatest, put, all, call, select } from 'redux-saga/effects';
-import { doc, getDoc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
+import {
+	doc,
+	getDoc,
+	updateDoc,
+	arrayUnion,
+	arrayRemove,
+} from 'firebase/firestore';
 
 import BooksActionTypes from './books.types';
+import { selectBookKey } from '../books/books.selectors';
+import { selectCurrentUser } from '../user/user.selectors';
 import {
-  fetchBookSuccess, 
-  fetchBookFailure, 
-  fetchBookStatusSuccess,
-  fetchBookStatusFailure,
-  updateBookStatusSuccess,
-  updateBookStatusFailure
+	fetchBookSuccess,
+	fetchBookFailure,
+	fetchBookStatusSuccess,
+	fetchBookStatusFailure,
+	updateBookStatusSuccess,
+	updateBookStatusFailure,
 } from './books.actions';
 import { db } from '../../firebase/firebase.utils';
-import { selectCurrentUser } from '../user/user.selectors';
-import { selectBookKey } from '../books/books.selectors';
 
 const statuses = ['wantToRead', 'read', 'currentlyReading'];
 const statusesAndAll = ['wantToRead', 'read', 'currentlyReading', 'all'];
 
-export function* fetchBookAsync({ payload }) {
-  try {
-    const response = yield fetch(`https://openlibrary.org/api/books?bibkeys=OLID:${payload}&jscmd=details&format=json`);
-    const json = yield response.json();
-    const book = yield json[Object.keys(json)[0]].details;
+function* getBookAuthors(book) {
+	if (book.authors) return book.authors;
 
-    book.description = '';
-    if (book.works) {
-      const worksResponse = yield fetch(`https://openlibrary.org${book.works[0].key}.json`);
-      const works = yield worksResponse.json();
-      if (!book.authors && !book.by_statement && works.authors) {
-        const authorResponse = yield fetch(`https://openlibrary.org${works.authors[0].author.key}.json`);
-        const author = yield authorResponse.json();
-        book.authors = [{ name: author.name }];
-      }
-      const description = works.description
-        ? works.description.value
-          ? works.description.value
-          : works.description
-        : '';
-      book.description = description;
-    }
-    yield put(fetchBookSuccess(book));
-  } catch (error) {
-    yield put(fetchBookFailure(error.message));
-  }
+	const works = yield getBookWorks(book);
+	if (!book.by_statement && works.authors) {
+		const authorResponse = yield fetch(
+			`https://openlibrary.org${works.authors[0].author.key}.json`
+		);
+		const author = yield authorResponse.json();
+		return [{ name: author.name }];
+	}
 }
 
-
-export function* fetchBookStatus({ payload: bookKey }) {
-  try {
-    const user = yield select(selectCurrentUser);
-    const userRef = doc(db, 'users', user.uid);
-    const userSnap = yield getDoc(userRef)
-    const userData = userSnap.data();
-    const userBooks = userData.books;
-    if (userBooks.all.includes(bookKey)) {
-      for (const status of statuses) {
-        if (userBooks[status].includes(bookKey))
-          yield put(fetchBookStatusSuccess(status));
-      }
-    } else {
-      yield put(fetchBookStatusSuccess(null));
-    }
-  } catch(error) {
-    yield put(fetchBookStatusFailure(error))
-  }
+function* getBookWorks(book) {
+	if (book.works) {
+		const worksResponse = yield fetch(
+			`https://openlibrary.org${book.works[0].key}.json`
+		);
+		const works = yield worksResponse.json();
+		return works;
+	}
+	return [];
 }
 
-export function* updateBookStatus({ payload: status }) {
-  try {
-    const user = yield select(selectCurrentUser);
-    const userRef = doc(db, 'users', user.uid);
-    const userSnap = yield getDoc(userRef)
-    const userData = userSnap.data();
-    const userBooks = userData.books;
-    const bookKey = yield select(selectBookKey);
+function* getBookDescription(book) {
+	const works = yield getBookWorks(book);
+	if (works) {
+		const description = works.description
+			? works.description.value
+				? works.description.value
+				: works.description
+			: '';
+		return description;
+	}
+	return '';
+}
 
-    if (status) {
-      for (const s of statuses)
-        if (s !== status && userBooks[s].includes(bookKey))
-          yield updateDoc(userRef, {
-            [`books.${s}`]: arrayRemove(bookKey)
-          });
-      yield updateDoc(userRef, {
-        'books.all': arrayUnion(bookKey),
-        [`books.${status}`]: arrayUnion(bookKey)
-      });
-    } else {
-      for (const s of statusesAndAll) {
-        if (userBooks[s].includes(bookKey))
-          yield updateDoc(userRef, {
-            [`books.${s}`]: arrayRemove(bookKey)
-          });
-      }
-    }
-    yield put(updateBookStatusSuccess(status));
-  } catch (error) {
-    yield put(updateBookStatusFailure(error));
-  }
+function* getUserRef() {
+	const user = yield select(selectCurrentUser);
+	return doc(db, 'users', user.uid);
+}
+
+function* getUserBooks() {
+	const userRef = yield getUserRef();
+	const userSnap = yield getDoc(userRef);
+	const userData = userSnap.data();
+	return userData.books;
+}
+
+function* fetchBookAsync({ payload }) {
+	try {
+		const response = yield fetch(
+			`https://openlibrary.org/api/books?bibkeys=OLID:${payload}&jscmd=details&format=json`
+		);
+		const json = yield response.json();
+		const book = yield json[Object.keys(json)[0]].details;
+		book.authors = yield getBookAuthors(book);
+		book.description = yield getBookDescription(book);
+		yield put(fetchBookSuccess(book));
+	} catch (error) {
+		yield put(fetchBookFailure(error.message));
+	}
+}
+
+function* fetchBookStatus({ payload: bookKey }) {
+	try {
+		const userBooks = yield getUserBooks();
+		if (userBooks.all.includes(bookKey)) {
+			for (const status of statuses)
+				if (userBooks[status].includes(bookKey))
+					yield put(fetchBookStatusSuccess(status));
+		} else yield put(fetchBookStatusSuccess(null));
+	} catch (error) {
+		yield put(fetchBookStatusFailure(error));
+	}
+}
+
+function* updateBookStatus({ payload: status }) {
+	try {
+		const userRef = yield getUserRef();
+		const userBooks = yield getUserBooks();
+		const bookKey = yield select(selectBookKey);
+		if (status) {
+			for (const s of statuses)
+				if (s !== status && userBooks[s].includes(bookKey))
+					yield updateDoc(userRef, {
+						[`books.${s}`]: arrayRemove(bookKey),
+					});
+			yield updateDoc(userRef, {
+				'books.all': arrayUnion(bookKey),
+				[`books.${status}`]: arrayUnion(bookKey),
+			});
+		} else {
+			for (const s of statusesAndAll) {
+				if (userBooks[s].includes(bookKey))
+					yield updateDoc(userRef, {
+						[`books.${s}`]: arrayRemove(bookKey),
+					});
+			}
+		}
+		yield put(updateBookStatusSuccess(status));
+	} catch (error) {
+		yield put(updateBookStatusFailure(error));
+	}
 }
 
 export function* onfetchBookStart() {
-  yield takeLatest(BooksActionTypes.FETCH_BOOK_START, fetchBookAsync);
+	yield takeLatest(BooksActionTypes.FETCH_BOOK_START, fetchBookAsync);
 }
 
 export function* onFetchBookStatusStart() {
-  yield takeLatest(BooksActionTypes.FETCH_BOOK_STATUS_START, fetchBookStatus);
+	yield takeLatest(BooksActionTypes.FETCH_BOOK_STATUS_START, fetchBookStatus);
 }
 
 export function* onUpdateBookStatusStart() {
-  yield takeLatest(BooksActionTypes.UPDATE_BOOK_STATUS_START, updateBookStatus);
+	yield takeLatest(BooksActionTypes.UPDATE_BOOK_STATUS_START, updateBookStatus);
 }
 
 export function* booksSagas() {
-  yield all([
-    call(onfetchBookStart),
-    call(onUpdateBookStatusStart),
-    call(onFetchBookStatusStart)
- ]);
+	yield all([
+		call(onfetchBookStart),
+		call(onUpdateBookStatusStart),
+		call(onFetchBookStatusStart),
+	]);
 }
