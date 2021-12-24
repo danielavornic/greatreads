2d09471bf3706d9e1d26d82d4d7e90727e221392
Daniela Vornic <daniela.vornic@gmail.com>
2021-12-24 17:29:32 +0200

    remove unnecessary yield keywords and format code in search sagas

--- src/redux/search/search.sagas.js
+++ src/redux/search/search.sagas.js
@@ -1,19 +1,18 @@
 import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import SearchActionTypes from './search.types';
-
 import { searchSuccess, searchFailure } from './search.actions';
 
 export function* searchAsync({ payload: { category, term, facet } }) {
   try {
-    const formattedTerm = yield term.replace(/ /g, '+').toLowerCase();
+    const formattedTerm = term.replace(/ /g, '+').toLowerCase();
     const urlFacet = (facet === 'all') ? 'q' : facet;
     const fetchUrl = (category === 'books') ? `https://openlibrary.org/search.json?${urlFacet}=${formattedTerm}`
                                             : `https://openlibrary.org/search/${category}.json?q=${formattedTerm}`;
     const response = yield fetch(fetchUrl);
     const searchResults = yield response.json();
     yield put(searchSuccess(searchResults));
-  } catch(error) {
+  } catch (error) {
     yield put(searchFailure(error.message));
   }
 }
