c6042b9aa18e385a96d4be87222efb1bf830a0a9
Daniela Vornic <daniela.vornic@gmail.com>
2022-01-04 20:47:12 +0200

    split header component in 2: for visitors and users

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import SignUpPage from './pages/signup';
 import UserBooksPage from './pages/user-books';
 
 import Header from './components/common/header';
+import HeaderUsers from './components/common/header-users';
 import Footer from './components/common/footer';
 
 import './App.css';
@@ -28,55 +29,54 @@ const ScrollToTop = () => {
   return null;
 };
 
-  return (
-    <div id='container'>
-      <Header />
-      <Container
-        px='0'
-        mt={{ base: 24, md: 28 }}
-        mb={{ base: 22, md: 24 }}
-        maxW={'full'}
-        id='main-content'
-      >
-        <ScrollToTop />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/signin/'>
-            {currentUser ? <Redirect to='/' /> : <SignInPage />}
-          </Route>
-          <Route path='/signup/'>
-            {currentUser ? <Redirect to='/' /> : <SignUpPage />}
-          </Route>
-          <Route
-            exact
-            path='/search/:category?/:term?/:facet?'
-            component={SearchPage}
-          />
-          <Route exact path='/books/'>
-            <Redirect to='/search/books/' />
-          </Route>
-          <Route exact path='/authors/'>
-            <Redirect to='/search/authors/' />
-          </Route>
-          <Route exact path='/books/:bookKey' component={BookPage} />
-          <Route exact path='/authors/:authorKey' component={AuthorPage} />
-          <Route exact path='/users/:username/books'>
-            <Redirect to='all' />
-          </Route>
-          <Route
-            exact
-            path='/users/:username/books/:shelf/'
-            component={UserBooksPage}
-          />
-        </Switch>
-      </Container>
-      <Footer />
-    </div>
-  );
-};
 const App = ({ currentUser }) => (
+  <div id='container'>
+    {currentUser ? <HeaderUsers /> : <Header />}
+    <Container
+      px='0'
+      mt={{ base: 24, md: 28 }}
+      mb={{ base: 22, md: 24 }}
+      maxW={'full'}
+      id='main-content'
+    >
+      <ScrollToTop />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/signin/'>
+          {currentUser ? <Redirect to='/' /> : <SignInPage />}
+        </Route>
+        <Route path='/signup/'>
+          {currentUser ? <Redirect to='/' /> : <SignUpPage />}
+        </Route>
+        <Route
+          exact
+          path='/search/:category?/:term?/:facet?'
+          component={SearchPage}
+        />
+        <Route exact path='/books/'>
+          <Redirect to='/search/books/' />
+        </Route>
+        <Route exact path='/authors/'>
+          <Redirect to='/search/authors/' />
+        </Route>
+        <Route exact path='/books/:bookKey' component={BookPage} />
+        <Route exact path='/authors/:authorKey' component={AuthorPage} />
+        <Route exact path='/users/:username/books'>
+          <Redirect to='all' />
+        </Route>
+        <Route
+          exact
+          path='/users/:username/books/:shelf/'
+          component={UserBooksPage}
+        />
+      </Switch>
+    </Container>
+    <Footer />
+  </div>
+);
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
+export default connect(mapStateToProps)(App);
---
+++ src/components/common/header-users.jsx
@@ -0,0 +1,135 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { Link } from 'react-router-dom';
+import {
+  chakra,
+  Flex,
+  Box,
+  VisuallyHidden,
+  HStack,
+  VStack,
+  Button,
+  Avatar,
+  Menu,
+  MenuGroup,
+  MenuItem,
+  MenuButton,
+  MenuList,
+  MenuDivider,
+  useDisclosure,
+  IconButton,
+  Text,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+import {
+  selectCurrentUser,
+  selectUsername,
+} from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
+
+import SearchInput from '../search/search-input';
+import { ReactComponent as Logo } from '../../assets/greatreads-logo.svg';
+
+const HeaderUsers = ({ currentUser, signOutStart, username }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <chakra.header
+      bg='rgba(255, 255, 255, 0.9)'
+      w='full'
+      px={{ base: 4, md: 8, lg: 10 }}
+      py={4}
+      borderBottom='1px'
+      borderColor='gray.200'
+      position='fixed'
+      zIndex='100'
+      backdropFilter='saturate(190%) blur(5px)'
+      boxShadow='xs'
+    >
+      <Flex alignItems='center' justifyContent='space-between' mx='auto'>
+        <IconButton
+          size={'md'}
+          variant={'ghost'}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label={'Open Menu'}
+          display={{ base: currentUser ? 'block' : 'none', md: 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack spacing={8} alignItems={'center'}>
+          <Link to='/'>
+            <Logo />
+            <VisuallyHidden>greatreads</VisuallyHidden>
+          </Link>
+          <HStack
+            as={'nav'}
+            spacing={6}
+            ms={12}
+            display={{ base: 'none', md: 'flex' }}
+          >
+            <Link to='/'>
+              <Text fontSize={14}>Home</Text>
+            </Link>
+            <Link to={`/users/${username}/books/all`}>
+              <Text fontSize={14}>My books</Text>
+            </Link>
+          </HStack>
+        </HStack>
+        <HStack spacing={4}>
+          <Box display={{ base: 'none', md: 'block' }}>
+            <SearchInput inputCategory='books' headerInput />
+          </Box>
+          <Menu bg={'white'}>
+            <MenuButton
+              as={Button}
+              rounded={'full'}
+              variant={'link'}
+              cursor={'pointer'}
+              minW={0}
+            >
+              <Avatar
+                size='sm'
+                bg={'brand.500'}
+                color={'white'}
+                name={currentUser.displayName}
+                src={currentUser.photoURL ? currentUser.photoURL : ''}
+              />
+            </MenuButton>
+            <MenuList>
+              <MenuGroup title={currentUser.displayName} pb={2}>
+                {/* <MenuItem>Profile</MenuItem> */}
+              </MenuGroup>
+              <MenuDivider />
+              <Link to='/'>
+                <MenuItem onClick={signOutStart}>Sign out</MenuItem>
+              </Link>
+            </MenuList>
+          </Menu>
+        </HStack>
+      </Flex>
+
+      {isOpen ? (
+        <Box pt={6} pb={4} display={{ md: 'none' }}>
+          <VStack as={'nav'} spacing={4}>
+            <SearchInput inputCategory='books' headerInput />
+            <Link to='/'>Home</Link>
+            <Link to={`/users/${currentUser.username}/books/all`}>
+              My books
+            </Link>
+          </VStack>
+        </Box>
+      ) : null}
+    </chakra.header>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  username: selectUsername,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  signOutStart: () => dispatch(signOutStart()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderUsers);
